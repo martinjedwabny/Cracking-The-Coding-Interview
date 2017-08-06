2509c082682a2c7661bbe6a78ef2d13a6ea18f3e
Martin Jedwabny <martiniedva@gmail.com>
2017-08-06 15:25:27 -0300

    [Chapter 03] Exercise 1

--- src/Chapter03.cpp
+++ src/Chapter03.cpp
@@ -7,6 +7,98 @@
 
 #include "Chapter03.h"
 
+//TESTS
+
 void testChapter03() {
-	cout << "a";
+	testTSA();
+}
+
+void testTSA() {
+	three_stack_array tsa = three_stack_array(3);
+	tsa.push(1,0);
+	tsa.push(2,0);
+	tsa.push(3,1);
+	tsa.push(4,2);
+	tsa.push(5,2);
+	tsa.push(6,2);
+	cout << tsa.top(0) << endl;
+	cout << tsa.top(1) << endl;
+	cout << tsa.top(2) << endl;
+	tsa.pop(0); tsa.pop(1); tsa.pop(2);
+	cout << tsa.top(0) << endl;
+	cout << tsa.top(1) << endl;
+	cout << tsa.top(2) << endl;
+	tsa.pop(0); tsa.pop(1); tsa.pop(2);
+	cout << tsa.top(0) << endl;
+	cout << tsa.top(1) << endl;
+	cout << tsa.top(2) << endl;
+	tsa.pop(0); tsa.pop(1); tsa.pop(2);
+}
+
+//STACK DATA STRUCTURE
+
+stack::stack() {
+	top_node = nullptr;
+}
+
+stack::stack(int* values, int n) {
+	//preconditions: values is a int array of size n > 0
+	top_node = nullptr;
+	for (int i = n-1; i >= 0; --i)
+		push(values[i]);
+}
+
+stack::~stack() {
+	while (top_node != nullptr)
+		pop();
+}
+
+void stack::push(int v) {
+	stack_node* n = new stack_node(v, top_node);
+	top_node = n;
+}
+
+void stack::pop() {
+	if (top_node != nullptr) {
+		stack_node* n = top_node;
+		top_node = n->substack;
+		delete n;
+	}
+}
+
+int stack::top() {
+	return top_node->value;
+}
+
+//THREE STACK ARRAY DATA STRUCTURE
+three_stack_array::three_stack_array(int n) {
+	array = new int[3 * n];
+	this->n = n;
+	size = new int[3];
+	size[0] = size[1] = size[2] = 0;
+}
+
+three_stack_array::~three_stack_array() {
+	delete array;
+}
+
+bool three_stack_array::push(int v, int stack) {
+	if (size[stack] >= n)
+		return false;
+	array[n * stack + size[stack]] = v;
+	size[stack]++;
+	return true;
+}
+
+void three_stack_array::pop(int stack) {
+	if (size[stack] == 0)
+		return;
+	size[stack]--;
+}
+
+int three_stack_array::top(int stack) {
+	int index = n * stack + size[stack] - 1;
+	if (size[stack]==0)
+		return -1;
+	return array[index];
 }
--- src/Chapter03.h
+++ src/Chapter03.h
@@ -13,6 +13,42 @@
 
 using namespace std;
 
+//STACK DATA STRUCTURE
+
+class stack {
+public:
+	class stack_node {
+	public:
+		int value;
+		stack_node* substack;
+		stack_node(int v, stack_node* s) {value = v; substack = s;}
+	};
+	stack_node* top_node;
+	stack();
+	stack(int* values, int n);
+	~stack();
+	void push(int v);
+	void pop();
+	int top();
+};
+
+//THREE STACK ARRAY DATA STRUCTURE
+
+class three_stack_array {
+public:
+	int* array;
+	int n;
+	int* size;
+	three_stack_array(int n);
+	~three_stack_array();
+	bool push(int v, int stack);
+	void pop(int stack);
+	int top(int stack);
+};
+
+//TESTS
+
 void testChapter03();
+void testTSA();
 
 #endif /* CHAPTER03_H_ */
